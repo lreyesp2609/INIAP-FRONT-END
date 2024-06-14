2ae25745a6de823405110cb20ea7d679955b4dfe
lreyesp2609 <lreyesp@uteq.edu.ec>
2024-06-13 23:59:14 -0500

    Menu lateral para el superusuario

--- src/components/SuperUsuario/lateralizquierdo.jsx
+++ src/components/SuperUsuario/lateralizquierdo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaUsers } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
 
 const LeftMenu = ({ user, onNavigate }) => {
   if (!user || !user.unidades || user.unidades.length === 0) {
@@ -7,16 +7,15 @@ const LeftMenu = ({ user, onNavigate }) => {
   }
 
   return (
-    <div className="bg-[#169658] text-white flex flex-col justify-between items-center fixed left-0 top-0 h-full z-40">
-      <div className="p-4 flex flex-col items-center h-full">
-        <div className="mb-4">
-          <FaUsers className="text-4xl" />
-        </div>
-        <div className="flex flex-col items-center space-y-4 w-full">
-          <div className="flex items-center cursor-pointer" onClick={() => onNavigate('gestion-empleados')}>
-            <span className="text-lg">Gestión de Empleados</span>
-          </div>
-        </div>
+    <div className="w-16 md:w-1/6 p-4 text-white flex flex-col justify-start items-center fixed left-0 top-0 h-full z-40 bg-[#169658]">
+      <div className="flex flex-col items-center space-y-6 mt-20 w-full">
+        <button 
+          onClick={() => onNavigate('gestion-empleados')} 
+          className="flex items-center w-full focus:outline-none hover:bg-[#0d4b34] p-2 rounded transition duration-200 ease-in-out"
+        >
+          <FiUsers className="w-8 h-8 text-white mx-4" />
+          <span className="text-white text-sm hidden md:inline">Gestión de Empleados</span>
+        </button>
       </div>
     </div>
   );
