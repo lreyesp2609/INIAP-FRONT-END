0d871377a022c59a13118de4b9a686b97c0cbbd5
RafaelNavas03 <84700397+RafaelNavas03@users.noreply.github.com>
2024-07-12 10:57:16 -0500

    arreeglos de la solicitud

--- src/components/Modulo Empleado/Solicitudes/CrearSolicitud.jsx
+++ src/components/Modulo Empleado/Solicitudes/CrearSolicitud.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import API_URL from '../../../Config';
 
@@ -10,48 +10,49 @@ const CrearSolicitud = ({ onClose, idEmpleado }) => {
   const [horaLlegada, setHoraLlegada] = useState('');
   const [actividades, setActividades] = useState('');
   const [listadoEmpleados, setListadoEmpleados] = useState('');
+  const [codigoSolicitud, setCodigoSolicitud] = useState('');
+  const [fechaHoraPrevisualizacion, setFechaHoraPrevisualizacion] = useState('');
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setError(null);
-  
-    // Validar y ajustar las fechas
-    const formattedFechaSalida = fechaSalida ? fechaSalida : null; // O ajusta según tu lógica
-    const formattedFechaLlegada = fechaLlegada ? fechaLlegada : null; // O ajusta según tu lógica
-  
+
+    const formattedFechaSalida = fechaSalida ? fechaSalida : null;
+    const formattedFechaLlegada = fechaLlegada ? fechaLlegada : null;
+
     const storedUser = JSON.parse(localStorage.getItem('user'));
     const idUsuario = storedUser.usuario.id_usuario;
-    
+
     const token = localStorage.getItem('token');
     if (!token) {
       setError('Token no encontrado');
       return;
     }
-  
+
     try {
       const response = await fetch(`${API_URL}/Informes/crear-solicitud/${idUsuario}/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `${token}`,
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
           motivo_movilizacion: motivo,
-          fecha_salida_solicitud: formattedFechaSalida, // Enviar la fecha formateada
+          fecha_salida_solicitud: formattedFechaSalida,
           hora_salida_solicitud: horaSalida,
-          fecha_llegada_solicitud: formattedFechaLlegada, // Enviar la fecha formateada
+          fecha_llegada_solicitud: formattedFechaLlegada,
           hora_llegada_solicitud: horaLlegada,
           descripcion_actividades: actividades,
           listado_empleado: listadoEmpleados,
           id_empleado: idEmpleado,
         }),
       });
-  
+
       if (response.ok) {
         onClose();
-        navigate('/menu-empleados'); // Redirigir a la lista de solicitudes
+        navigate('/menu-empleados');
       } else {
         const errorData = await response.json();
         setError(errorData.error || 'Error al crear la solicitud');
@@ -60,14 +61,71 @@ const CrearSolicitud = ({ onClose, idEmpleado }) => {
       setError('Error al crear la solicitud: ' + error.message);
     }
   };
-  
-  
+
+  const fetchPrevisualizacion = async () => {
+    try {
+      const storedUser = JSON.parse(localStorage.getItem('user'));
+      const idUsuario = storedUser.usuario.id_usuario;
+      const token = localStorage.getItem('token');
+
+      if (!token) {
+        setError('Token no encontrado');
+        return;
+      }
+
+      const response = await fetch(`${API_URL}/Informes/previsualizar-codigo-solicitud/${idUsuario}/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.previsualizacion) {
+          setCodigoSolicitud(data.previsualizacion['Codigo de Solicitud'] || '');
+          setFechaHoraPrevisualizacion(data.previsualizacion['Fecha y Hora de Previsualización'] || '');
+        } else {
+          setError('Datos de previsualización no encontrados');
+        }
+      } else {
+        setError('Error al obtener la previsualización');
+      }
+    } catch (error) {
+      setError('Error al obtener la previsualización: ' + error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchPrevisualizacion();
+  }, []);
 
   return (
     <div className="p-4">
-      <h2 className="text-xl font-semibold mb-4">Nueva Solicitud</h2>
+      <h2 className="text-xl font-semibold mb-4">Crear Nueva Solicitud</h2>
+      <label className="block text-gray-700 text-sm font-bold mb-2">{'\u00A0'}</label>
       {error && <div className="mb-4 text-red-500">{error}</div>}
       <form onSubmit={handleSubmit}>
+        <div className="mb-4 flex">
+          <div className="mr-4 w-1/2">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Nro. SOLICITUD DE AUTORIZACIÓN PARA CUMPLIMIENTO DE SERVICIOS INSTITUCIONALES</label>
+            <input
+              type="text"
+              value={codigoSolicitud}
+              readOnly
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div className="w-1/2">
+            <label className="block text-gray-700 text-sm font-bold mb-2">FECHA DE SOLICITUD (dd-mmm-aaa)</label>
+            <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'}</label>
+            <input
+              type="text"
+              value={fechaHoraPrevisualizacion}
+              readOnly
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+        </div>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">Motivo Movilización</label>
           <input
