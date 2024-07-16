6661d650e86e68449cdda9f1d468b92b72e97761
RafaelNavas03 <84700397+RafaelNavas03@users.noreply.github.com>
2024-07-16 01:39:04 -0500

    solo falta lista de empleados

--- src/components/Modulo Empleado/Solicitudes/CrearSolicitud.jsx
+++ src/components/Modulo Empleado/Solicitudes/CrearSolicitud.jsx
@@ -203,190 +203,207 @@ const CrearSolicitud = ({ onClose, idEmpleado }) => {
 
   return (
     <div className="p-4">
-      <h2 className="block text-gray-700 text-sm font-bold mb-2 text-center">
-        SOLICITUD DE AUTORIZACIÓN PARA CUMPLIMIENTO DE SERVICIOS INSTITUCIONALES
-      </h2>
-      <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
-      <label className="block text-gray-700 text-sm font-bold mb-2">{'\u00A0'}</label>
-      {error && <div className="mb-4 text-red-500">{error}</div>}
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4 flex">
-          <div className="mr-4 w-1/2">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Nro. SOLICITUD DE AUTORIZACIÓN PARA CUMPLIMIENTO DE SERVICIOS INSTITUCIONALES
-            </label>
+  <h2 className="block text-gray-700 text-sm font-bold mb-2 text-center">
+    SOLICITUD DE AUTORIZACIÓN PARA CUMPLIMIENTO DE SERVICIOS INSTITUCIONALES
+  </h2>
+  <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+  <label className="block text-gray-700 text-sm font-bold mb-2">{'\u00A0'}</label>
+  {error && <div className="mb-4 text-red-500">{error}</div>}
+  <form onSubmit={handleSubmit}>
+    <div className="mb-4 flex">
+      <div className="mr-4 w-1/2">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Nro. SOLICITUD DE AUTORIZACIÓN PARA CUMPLIMIENTO DE SERVICIOS INSTITUCIONALES
+        </label>
+        <input
+          type="text"
+          value={codigoSolicitud}
+          readOnly
+          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <div className="w-1/2">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          FECHA DE SOLICITUD (dd-mmm-aaa)
+        </label>
+        <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+        <input
+          type="text"
+          value={fechaHoraPrevisualizacion}
+          readOnly
+          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+    </div>
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2">MOTIVO MOVILIZACIÓN</label>
+      <select
+        value={motivo}
+        onChange={(e) => setMotivo(e.target.value)}
+        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        required
+      >
+        <option value="">Selecciona un motivo...</option>
+        {motivos.map((motivo, index) => (
+          <option key={index} value={motivo}>
+            {motivo}
+          </option>
+        ))}
+      </select>
+    </div>
+    <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+    <h2 className="block text-gray-700 text-sm font-bold mb-2 text-center">
+      DATOS GENERALES
+    </h2>
+    <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+    {datosPersonales && (
+      <div className="mb-4 flex">
+      <div className="w-10/2 mr-4">
+        <div className="flex mb-4">
+          <div className="w-10/2 mr-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">APELLIDOS - NOMBRES DE LA O EL SERVIDOR</label>
             <input
               type="text"
-              value={codigoSolicitud}
+              value={`${datosPersonales.Nombre}`}
               readOnly
               className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
           <div className="w-1/2">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              FECHA DE SOLICITUD (dd-mmm-aaa)
-            </label>
-            <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+            <label className="block text-gray-700 text-sm font-bold mb-2">PUESTO QUE OCUPA:</label>
             <input
               type="text"
-              value={fechaHoraPrevisualizacion}
+              value={`${datosPersonales.Cargo}`}
               readOnly
               className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">MOTIVO MOVILIZACIÓN</label>
-          <select
-            value={motivo}
-            onChange={(e) => setMotivo(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          >
-            <option value="">Selecciona un motivo...</option>
-            {motivos.map((motivo, index) => (
-              <option key={index} value={motivo}>
-                {motivo}
-              </option>
-            ))}
-          </select>
-        </div>
-        <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
-        <h2 className="block text-gray-700 text-sm font-bold mb-2 text-center">
-          DATOS GENERALES
-        </h2>
-        <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
-        {datosPersonales && (
-          <div className="mb-4 flex">
-            <div className="mr-4 w-1/2">
-              <label className="block text-gray-700 text-sm font-bold mb-2">APELLIDOS - NOMBRES DE LA O EL SERVIDOR</label>
-              <input
-                type="text"
-                value={`${datosPersonales.Nombre}`}
-                readOnly
-                className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2"
-              />
-              <label className="block text-gray-700 text-sm font-bold mb-2">PUESTO QUE OCUPA:</label>
-              <input
-                type="text"
-                value={`${datosPersonales.Cargo}`}
-                readOnly
-                className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2"
-              />
-              <div className="mb-4 flex">
-                <div className="mr-4 w-1/2">
-                  <label className="block text-gray-700 text-sm font-bold mb-2">Provincia</label>
-                  <select
-                    value={selectedProvincia}
-                    onChange={handleProvinciaChange}
-                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  >
-                    <option value="">Seleccionar Provincia</option>
-                    {provincias.map((p) => (
-                      <option key={p.Provincia} value={p.Provincia}>
-                        {p.Provincia}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className="w-1/2">
-                  <label className="block text-gray-700 text-sm font-bold mb-2">Ciudad</label>
-                  <select
-                    value={selectedCiudad}
-                    onChange={(e) => setSelectedCiudad(e.target.value)}
-                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  >
-                    <option value="">Seleccionar Ciudad</option>
-                    {ciudades.map((c) => (
-                      <option key={c} value={c}>
-                        {c}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-              <label className="block text-gray-700 text-sm font-bold mb-2">NOMBRE DE LA UNIDAD A LA QUE PERTENECE LA O EL SERVIDOR</label>
-              <input
-                type="text"
-                value={`${datosPersonales.Unidad}`}
-                readOnly
-                className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
-            </div>
-          </div>
-        )}
         <div className="mb-4 flex">
-          <div className="mr-4 w-1/4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">Fecha Salida</label>
-            <input
-              type="date"
-              value={fechaSalida}
-              onChange={(e) => setFechaSalida(e.target.value)}
+        <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+          <div className="w-1/2 mr-4">
+          <label className="block text-gray-700 text-sm font-bold mb-1/2">CIUDAD - PROVINCIA DEL SERVICIO INSTITUCIONAL:</label>
+          <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+            <label className="block text-gray-700 text-sm font-bold mb-2">PROVINCIA:</label>
+            <select
+              value={selectedProvincia}
+              onChange={handleProvinciaChange}
               className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
+            >
+              <option value="">Seleccionar Provincia</option>
+              {provincias.map((p) => (
+                <option key={p.Provincia} value={p.Provincia}>
+                  {p.Provincia}
+                </option>
+              ))}
+            </select>
           </div>
-          <div className="mr-4 w-1/4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">Hora Salida</label>
-            <input
-              type="time"
-              value={horaSalida}
-              onChange={(e) => setHoraSalida(e.target.value)}
+          <div className="w-1/2 mr-4">
+          <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+          <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+          <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+            <label className="block text-gray-700 text-sm font-bold mb-2">CIUDAD:</label>
+            <select
+              value={selectedCiudad}
+              onChange={(e) => setSelectedCiudad(e.target.value)}
               className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
+            >
+              <option value="">Seleccionar Ciudad</option>
+              {ciudades.map((c) => (
+                <option key={c} value={c}>
+                  {c}
+                </option>
+              ))}
+            </select>
           </div>
-          <div className="mr-4 w-1/4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">Fecha Llegada</label>
+          <div className="w-1/2">
+          <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+          <label className="block text-gray-700 text-sm font-bold mb-1/2">{'\u00A0'} {/* Espacio en blanco */}</label>
+            <label className="block text-gray-700 text-sm font-bold mb-2">NOMBRE DE LA UNIDAD A LA QUE PERTENECE LA O EL SERVIDOR</label>
             <input
-              type="date"
-              value={fechaLlegada}
-              onChange={(e) => setFechaLlegada(e.target.value)}
+              type="text"
+              value={`${datosPersonales.Unidad}`}
+              readOnly
               className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
-          </div>
-          <div className="w-1/4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">Hora Llegada</label>
-            <input
-              type="time"
-              value={horaLlegada}
-              onChange={(e) => setHoraLlegada(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
             />
           </div>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Descripción Actividades</label>
-          <textarea
-            value={actividades}
-            onChange={(e) => setActividades(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-            rows="4"
-            required
-          ></textarea>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Listado Empleados</label>
-          <textarea
-            value={listadoEmpleados}
-            onChange={(e) => setListadoEmpleados(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-            rows="4"
-            required
-          ></textarea>
-        </div>
-        <div className="flex justify-between">
-          <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-            Enviar
-          </button>
-          <button type="button" onClick={onClose} className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">
-            Cancelar
-          </button>
-        </div>
-      </form>
+      </div>
     </div>
+    
+    )}
+    <div className="mb-4 flex">
+      <div className="mr-4 w-1/4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">Fecha Salida</label>
+        <input
+          type="date"
+          value={fechaSalida}
+          onChange={(e) => setFechaSalida(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+      </div>
+      <div className="mr-4 w-1/4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">Hora Salida</label>
+        <input
+          type="time"
+          value={horaSalida}
+          onChange={(e) => setHoraSalida(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+      </div>
+      <div className="mr-4 w-1/4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">Fecha Llegada</label>
+        <input
+          type="date"
+          value={fechaLlegada}
+          onChange={(e) => setFechaLlegada(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+      </div>
+      <div className="w-1/4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">Hora Llegada</label>
+        <input
+          type="time"
+          value={horaLlegada}
+          onChange={(e) => setHoraLlegada(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+      </div>
+    </div>
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2">SERVIDORES QUE INTEGRAN LOS SERVICIOS INSTITUCIONALES:</label>
+      <textarea
+        value={listadoEmpleados}
+        onChange={(e) => setListadoEmpleados(e.target.value)}
+        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        rows="4"
+        required
+      ></textarea>
+    </div>
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2">DESCRIPCIÓN DE LAS ACTIVIDADES A EJECUTARSE</label>
+      <textarea
+        value={actividades}
+        onChange={(e) => setActividades(e.target.value)}
+        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        rows="4"
+        required
+      ></textarea>
+    </div>
+    <div className="flex justify-between">
+      <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        Enviar
+      </button>
+      <button type="button" onClick={onClose} className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">
+        Cancelar
+      </button>
+    </div>
+  </form>
+</div>
   );
 };
 
