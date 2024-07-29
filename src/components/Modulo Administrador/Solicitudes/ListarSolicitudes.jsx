3afbc4ccac6aa63eba95e8dc314f6234623b2640
RafaelNavas03 <84700397+RafaelNavas03@users.noreply.github.com>
2024-07-29 08:36:45 -0500

    lista solicitudes pendientes admin

--- src/components/Modulo Administrador/Solicitudes/ListarSolicitudes.jsx
+++ src/components/Modulo Administrador/Solicitudes/ListarSolicitudes.jsx
@@ -1,178 +1,199 @@
 import React, { useState, useEffect } from 'react';
-import { FaBan, FaEye, FaCheck, FaEdit, FaFilePdf } from 'react-icons/fa';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faEye } from '@fortawesome/free-solid-svg-icons';
 import API_URL from '../../../Config';
 
 
 const ListarSolicitudes = () => {
-    const [solicitudes, setSolicitudes] = useState([]);
-    const [filteredSolicitudes, setFilteredSolicitudes] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage] = useState(10);
-  
-    useEffect(() => {
-      fetchSolicitudes();
-    }, []);
-  
-    const fetchSolicitudes = async () => {
-      try {
-        const storedUser = JSON.parse(localStorage.getItem('user'));
-        const idUsuario = storedUser.usuario.id_usuario;
-  
-        const token = localStorage.getItem('token');
-        if (!token) throw new Error('Token no encontrado');
-  
-        const response = await fetch(`${API_URL}/Informes/listar-solicitudes/${idUsuario}/`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        if (!response.ok) throw new Error('Error al obtener solicitudes');
-  
-        const data = await response.json();
-        setSolicitudes(data.solicitudes);
-        setFilteredSolicitudes(data.solicitudes);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
+  const [solicitudes, setSolicitudes] = useState([]);
+  const [filteredSolicitudes, setFilteredSolicitudes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+  const [isCreating, setIsCreating] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('pendientes');
+
+  useEffect(() => {
+    fetchSolicitudes();
+  }, [selectedOption]);
+
+  const fetchSolicitudes = async () => {
+    try {
+      const storedUser = JSON.parse(localStorage.getItem('user'));
+      const idUsuario = storedUser.usuario.id_usuario;
+      const token = localStorage.getItem('token');
+      if (!token) throw new Error('Token no encontrado');
+
+      let url;
+      if (selectedOption === 'pendientes') {
+        url = `${API_URL}/Informes/listar-solicitudes-pedientes-admin/`;
+      } else if (selectedOption === 'aceptadas') {
+        url = `${API_URL}/Informes/listar-solicitudes-aceptadas/${idUsuario}/`;
+      } else if (selectedOption === 'canceladas') {
+        url = `${API_URL}/Informes/listar-solicitudes-canceladas/${idUsuario}/`;
       }
-    };
-  
-    const handleSearch = (event) => {
-      const searchValue = event.target.value.toLowerCase();
-      setSearchTerm(event.target.value);
-  
-      const filtered = solicitudes.filter(
-        (solicitud) =>
-          solicitud['Codigo de Solicitud'].toLowerCase().includes(searchValue) ||
-          solicitud['Fecha Solicitud'].toLowerCase().includes(searchValue) ||
-          solicitud['Motivo'].toLowerCase().includes(searchValue) ||
-          solicitud['Estado'].toLowerCase().includes(searchValue)
-      );
-  
-      setFilteredSolicitudes(filtered);
-      setCurrentPage(1);
-    };
-  
-    const handleClear = () => {
-      setSearchTerm('');
-      setFilteredSolicitudes(solicitudes);
-      setCurrentPage(1);
-    };
-  
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = filteredSolicitudes.slice(indexOfFirstItem, indexOfLastItem);
-    const totalPages = Math.ceil(filteredSolicitudes.length / itemsPerPage);
-  
-    const handleClick = (pageNumber) => {
-      setCurrentPage(pageNumber);
-    };
-  
-  
-    return (
-      <div className="p-4">
-            <div className="flex justify-between items-center mb-4">
-              <h1 className="text-2xl font-light">Solicitudes de Viajes</h1>
-            </div>
-            <div className="mb-4">
-              <div className="flex">
-                <input
-                  type="text"
-                  placeholder="Buscar por número, motivo o estado"
-                  value={searchTerm}
-                  onChange={handleSearch}
-                  className="w-full p-2 border border-gray-300 rounded-l focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <button
-                  className="px-4 py-2 bg-blue-500 text-white rounded-r hover:bg-blue-600"
-                  onClick={handleClear}
-                  style={{ minWidth: '80px' }}
-                >
-                  Limpiar
-                </button>
-              </div>
-            </div>
-            <div className="overflow-x-auto">
-              <table className="min-w-full bg-white border border-gray-300">
-                <thead>
-                  <tr className="w-full bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                    <th className="py-3 px-6 text-left">Código de Solicitud</th>
-                    <th className="py-3 px-6 text-left">Fecha Solicitud</th>
-                    <th className="py-3 px-6 text-left">Motivo Movilización</th>
-                    <th className="py-3 px-6 text-left">Estado Solicitud</th>
-                    <th className="py-3 px-6 text-left">Acciones</th>
-                  </tr>
-                </thead>
-                <tbody className="text-gray-600 text-sm font-light">
-                  {currentItems.map((solicitud) => (
-                    <tr key={solicitud['Codigo de Solicitud']} className="border-b border-gray-300 hover:bg-gray-100">
-                      <td className="py-3 px-6 text-left whitespace-nowrap">{solicitud['Codigo de Solicitud']}</td>
-                      <td className="py-3 px-6 text-left">{solicitud['Fecha Solicitud']}</td>
-                      <td className="py-3 px-6 text-left">{solicitud['Motivo']}</td>
-                      <td className="py-3 px-6 text-left">{solicitud['Estado']}</td>
-                      <td className="px-4 py-2 text-sm text-gray-600 flex space-x-2">
-                        <button 
-                        className="p-2 bg-yellow-500 text-white rounded-full"
-                        title="Ver"
-                        >
-                            <FaEye />
-                        </button>
-                        {solicitud.estado_movilizacion === 'Pendiente' && (
-                            <>
-                            <button 
-                            className="p-2 bg-green-500 text-white rounded-full"
-                            title="Aceptar"
-                            onClick={() => handleAccept(solicitud.id_orden_movilizacion)}
-                            >
-                                <FaCheck />
-                            </button>
-                            <button 
-                            className="p-2 bg-red-500 text-white rounded-full"
-                            title="Rechazar"
-                            onClick={() => handleReject(solicitud.id_orden_movilizacion)}
-                            >
-                                <FaBan />
-                            </button>
-                            </>
-                        )}
-                        {solicitud.estado_movilizacion === 'Aprobado' && (
-                        <button
-                        className="p-2 bg-red-500 text-white rounded-full"
-                        title="Exportar PDF"
-                        onClick={() => handlePDF(solicitud.id_orden_movilizacion)}
-                        >
-                            <FaFilePdf />
-                        </button>
-                        )}
-                    </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-            <div className="flex justify-between items-center mt-4">
-              <button
-                onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-                className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400"
-                disabled={currentPage === 1}
-              >
-                Anterior
-              </button>
-              <span>{`Página ${currentPage} de ${totalPages}`}</span>
-              <button
-                onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
-                className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400"
-                disabled={currentPage === totalPages}
-              >
-                Siguiente
-              </button>
-            </div>
-      </div>
-    );
+
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) throw new Error('Error al obtener solicitudes');
+
+      const data = await response.json();
+      setSolicitudes(data.solicitudes);
+      setFilteredSolicitudes(data.solicitudes);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
   };
-  
-  export default ListarSolicitudes;
+
+  const handleSearch = (event) => {
+    const searchValue = event.target.value.toLowerCase();
+    setSearchTerm(event.target.value);
+
+    const filtered = solicitudes.filter(
+      (solicitud) =>
+        solicitud['Codigo de Solicitud'].toLowerCase().includes(searchValue) ||
+        solicitud['Fecha Solicitud'].toLowerCase().includes(searchValue) ||
+        solicitud['Motivo'].toLowerCase().includes(searchValue) ||
+        solicitud['Estado'].toLowerCase().includes(searchValue)
+    );
+
+    setFilteredSolicitudes(filtered);
+    setCurrentPage(1);
+  };
+
+  const handleClear = () => {
+    setSearchTerm('');
+    setFilteredSolicitudes(solicitudes);
+    setCurrentPage(1);
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredSolicitudes.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(filteredSolicitudes.length / itemsPerPage);
+
+  const handleClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleCreateSolicitud = () => {
+    setIsCreating(true);
+  };
+
+  const handleCloseCreateSolicitud = () => {
+    setIsCreating(false);
+    fetchSolicitudes(); // Volver a cargar las solicitudes después de crear una nueva
+  };
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+    setCurrentPage(1);
+  };
+
+  return (
+    <div className="p-4">
+      {isCreating ? (
+        <CrearSolicitud onClose={handleCloseCreateSolicitud} />
+      ) : (
+        <>
+          <div className="flex justify-between items-center mb-4">
+            <h1 className="text-2xl font-light">Solicitudes de Viaje</h1>
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              onClick={handleCreateSolicitud}
+            >
+              Crear Solicitud
+            </button>
+          </div>
+          <div className="mb-4">
+          <label htmlFor="viewModeSelect" className="mr-2">Ver:</label>
+            <select 
+              value={selectedOption} 
+              onChange={handleOptionChange} 
+              className="px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            >
+              <option value="pendientes">Solicitudes Pendientes</option>
+              <option value="aceptadas">Solicitudes Aceptadas</option>
+              <option value="canceladas">Solicitudes Canceladas</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <div className="flex">
+              <input
+                type="text"
+                placeholder="Buscar por número, motivo o estado"
+                value={searchTerm}
+                onChange={handleSearch}
+                className="w-full p-2 border border-gray-300 rounded-l focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <button
+                className="px-4 py-2 bg-blue-500 text-white rounded-r hover:bg-blue-600"
+                onClick={handleClear}
+                style={{ minWidth: '80px' }}
+              >
+                Limpiar
+              </button>
+            </div>
+          </div>
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-white border border-gray-300">
+              <thead>
+                <tr className="w-full bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                  <th className="py-3 px-6 text-left">Código de Solicitud</th>
+                  <th className="py-3 px-6 text-left">Fecha Solicitud</th>
+                  <th className="py-3 px-6 text-left">Motivo Movilización</th>
+                  <th className="py-3 px-6 text-left">Estado Solicitud</th>
+                  <th className="py-3 px-6 text-left">Acciones</th>
+                </tr>
+              </thead>
+              <tbody className="text-gray-600 text-sm font-light">
+                {currentItems.map((solicitud) => (
+                  <tr key={solicitud['Codigo de Solicitud']} className="border-b border-gray-300 hover:bg-gray-100">
+                    <td className="py-3 px-6 text-left whitespace-nowrap">{solicitud['Codigo de Solicitud']}</td>
+                    <td className="py-3 px-6 text-left">{solicitud['Fecha Solicitud']}</td>
+                    <td className="py-3 px-6 text-left">{solicitud['Motivo']}</td>
+                    <td className="py-3 px-6 text-left">{solicitud['Estado']}</td>
+                    <td className="py-3 px-6 text-left">
+                      <button className="text-blue-500 hover:text-blue-700 mr-2">
+                        <FontAwesomeIcon icon={faEye} />
+                      </button>
+                      <button className="text-red-500 hover:text-red-700">
+                        <FontAwesomeIcon icon={faTrashAlt} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="flex justify-between items-center mt-4">
+            <button
+              onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+              className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400"
+              disabled={currentPage === 1}
+            >
+              Anterior
+            </button>
+            <span>{`Página ${currentPage} de ${totalPages}`}</span>
+            <button
+              onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+              className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400"
+              disabled={currentPage === totalPages}
+            >
+              Siguiente
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ListarSolicitudes;
