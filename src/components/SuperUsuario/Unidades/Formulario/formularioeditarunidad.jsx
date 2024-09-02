705689738d4fb1e6d539030155e23de7d97f21d8
Bryan Eduardo Robalino Chuez <eduardochuez_2003@outlook.com>
2024-09-02 12:35:41 -0500

    editar unidad

---
+++ src/components/SuperUsuario/Unidades/Formulario/formularioeditarunidad.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+const FormularioEditarUnidad = ({ unidad, onSubmit, onCancel}) => {
+  const [nombreUnidad, setnombreUnidad] = useState(unidad.nombre_unidad);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({
+      nombre_unidad: nombreUnidad,
+    });
+  };
+
+  return (
+    <div className="p-4">
+  <form onSubmit={handleSubmit} className="space-y-4">
+    <h3 className="text-xl font-semibold mb-4">Datos de la Unidad</h3>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div>
+        <label className="block text-sm font-medium text-gray-700">Nombre de la Unidad</label>
+        <input
+          type="text"
+          value={nombreUnidad}
+          onChange={(e) => setnombreUnidad(e.target.value)}
+          className="w-full bg-blue-100 text-black border border-blue-100 rounded py-2 px-4"
+          required
+        />
+      </div>
+    </div>
+    <div className="mt-8 flex flex-col md:flex-row justify-end md:space-x-4 space-y-4 md:space-y-0">
+      <button
+        type="submit"
+        className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 
+        border-b-4 border-blue-300 hover:border-blue-700 rounded
+        "
+      >
+       <FontAwesomeIcon icon={faSave} /> Guardar
+      </button>
+      <button
+        type="button"
+        onClick={onCancel}
+        className="w-full bg-red-600 hover:bg-red-700 text-white font-bold py-2 
+        px-4 border-b-4 border-red-400 hover:border-red-900 rounded"
+      >
+        <FontAwesomeIcon icon={faTimes} /> Cancelar
+      </button>
+    </div>
+  </form>
+</div>
+
+  );
+};
+
+export default FormularioEditarUnidad;
---
+++ src/components/SuperUsuario/Unidades/editarunidad.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import FormularioEditarunidad from '../Unidades/Formulario/formularioeditarunidad';
+import API_URL from "../../../Config";
+import { notification } from "antd";
+
+const EditarUnidad = ({ unidad, onCancel, onActualizacion }) => {
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const id_usuario = JSON.parse(atob(token.split(".")[1])).id_usuario;
+      setUserId(id_usuario);
+    }
+  }, []);
+
+  const handleSubmit = async (formData) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return;
+    }
+
+    const urlEncodedData = new URLSearchParams();
+    urlEncodedData.append("nombre_unidad", formData.nombre_unidad);
+;
+
+    try {
+      const response = await fetch(
+        `${API_URL}/Unidades/editar-unidades/${userId}/${unidad.id_unidad}/`,
+        {
+          method: 'POST',
+          headers: {
+            'Authorization': token,
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: urlEncodedData.toString()
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        notification.success({
+          message: 'Éxito',
+          description: data.mensaje || 'Estación editada correctamente'
+        });
+        onActualizacion();
+        onCancel();
+      } else {
+        const errorData = await response.json();
+        notification.error({
+          message: 'Error',
+          description: errorData.error || 'Error al editar la estación'
+        });
+        console.error('Error al actualizar la estación:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error al actualizar la estación:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Editar Estación</h2>
+      <FormularioEditarunidad
+        unidad={unidad}
+        onSubmit={handleSubmit}
+        onCancel={onCancel}
+      />
+    </div>
+  );
+};
+
+export default EditarUnidad;
